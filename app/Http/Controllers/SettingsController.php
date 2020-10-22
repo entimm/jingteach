93f12916dfe27e3ea25887a3a60218afeee97ca8
enjoy.liu <enjoy.liu@huolala.cn>
2020-10-23 00:30:39 +0800

    UPDATE

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Redis;
 
 class SettingsController extends Controller
@@ -26,13 +27,18 @@ class SettingsController extends Controller
 
     public function update(Request $request)
     {
+        $settings = array_filter(Arr::only($request->all(), ['t1', 't2', 't3', 'n', 'nn']), function ($item) {
+            return $item === null;
+        });
+        $settings = array_merge(Redis::hgetall('settings'), $settings);
         $settings = [
-            't1' => $request->input('t1', 100),
-            't2' => $request->input('t2', 400),
-            't3' => $request->input('t3', 1700),
-            'n' => $request->input('n', 10),
+            't1' => $settings['t1'] ?? 100,
+            't2' => $settings['t2'] ?? 400,
+            't3' => $settings['t3'] ?? 1700,
+            'n' => $settings['n'] ?? 10,
             'nn' => $settings['nn'] ?? 0,
         ];
+
         Redis::hmset('settings', $settings);
 
         echo '<pre>';
